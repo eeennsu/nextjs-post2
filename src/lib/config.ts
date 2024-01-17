7972a26455a00839ada6713ab8eae5c9d97ed84b
eeennsu <116697733+eeennsu@users.noreply.github.com>
2024-01-17 22:39:40 +0900

    url config 수정

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+export const API_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_API_URL! : process.env.NEXT_DEPLOY_API_URL;
