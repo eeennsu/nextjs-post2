91d6c0d3e4c8f74ba64269964ea9081cf605e986
eeennsu <116697733+eeennsu@users.noreply.github.com>
2024-01-18 12:44:53 +0900

    /api 추가

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '' : process.env.NEXT_PUBLIC_API_URL!;
+export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : process.env.NEXT_PUBLIC_API_URL!;
 
 export const headers = {
     'Accept': 'application/json',
