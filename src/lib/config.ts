770eec0fefe19bb93f70cb13fa8160f3250f0d4d
eeennsu <116697733+eeennsu@users.noreply.github.com>
2024-01-18 11:59:25 +0900

    config 수정

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_API_URL! : process.env.NEXT_DEPLOY_API_URL;
+export const API_URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_DEPLOY_API_URL! : process.env.NEXT_PUBLIC_API_URL!;
 
 export const headers = {
     'Accept': 'application/json',
